2ff7926fcbdd48b793ef7abceae6e49a398216ae
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-08 15:42:59 +0530

    Create solution_c3_saa2.py

---
+++ solution_c3_saa2.py
@@ -0,0 +1,12 @@
+# Create a variable 'x' 
+x = 10
+# Create a variable 'y' 
+y = 20
+# Printing the values of 'x' and 'y' before swapping
+print("Before Swapping:",x,y)
+
+# Swapping - Interchanging the values
+x,y = y,x
+
+# Printing the values of 'x' and 'y' after swapping
+print("After Swapping:",x,y)
